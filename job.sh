4870f94626d8f39f88c1654675a6f63acc3f5d03
Rayan Syed <rsyed@bu.edu>
2024-07-03 16:00:59 -0400

    atomically savve checkpoints

--- job.sh
+++ job.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Note how many times you want job to repeat
+# Total Training Time = NUM_JOB * HOURS_PER_JOB
 NUM_JOBS=3
+HOURS_PER_JOB=1 # Whole number, Must be less than 48
 
 BASE_JOB_NAME="automatic_training"
 
@@ -9,6 +10,6 @@ BASE_JOB_NAME="automatic_training"
 for i in $(seq 1 $NUM_JOBS); do
     job_name="${BASE_JOB_NAME}_${i}"
     previous_job_name="${BASE_JOB_NAME}_$((i-1))"
-    echo "starting job ${i}:"
-    qsub -N $job_name -hold_jid $previous_job_name -j y -o "/projectnb/tianlabdl/rsyed/automatic-training/logs/${BASE_JOB_NAME}_${i}.qlog" "job.qsub"
+    echo "Starting job ${i}:"
+    qsub -hold_jid $previous_job_name -N $job_name -o "/projectnb/tianlabdl/rsyed/automatic-training/logs/${BASE_JOB_NAME}_${i}.qlog" -l h_rt=${HOURS_PER_JOB}:00:00 "job.qsub"
 done
