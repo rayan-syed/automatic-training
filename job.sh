1a5c646c7e9532b0e8f0810720c33677f5f6ddae
Rayan Syed <rsyed@bu.edu>
2024-07-10 16:51:08 -0400

    Merge log files

--- job.sh
+++ job.sh
@@ -2,7 +2,7 @@
 
 # Total Training Time = NUM_JOB * HOURS_PER_JOB
 NUM_JOBS=3
-HOURS_PER_JOB=1 
+HOURS_PER_JOB=12
 LOG_DIRECTORY="/projectnb/tianlabdl/rsyed/automatic-training/logs/"
 
 BASE_JOB_NAME="automatic_training"
@@ -11,6 +11,8 @@ BASE_JOB_NAME="automatic_training"
 for i in $(seq 1 $NUM_JOBS); do
     job_name="${BASE_JOB_NAME}_${i}"
     previous_job_name="${BASE_JOB_NAME}_$((i-1))"
-    echo "Starting job ${i}:"
     qsub -hold_jid $previous_job_name -j y -N $job_name -o "${LOG_DIRECTORY}${BASE_JOB_NAME}_${i}.qlog" -l h_rt=${HOURS_PER_JOB}:00:00 "job.qsub"
 done
+
+# Append all log files in the end
+qsub -hold_jid "${BASE_JOB_NAME}_${NUM_JOBS}" -j y -N "append_${BASE_JOB_NAME}" -o "${LOG_DIRECTORY}append.qlog" -v mainlog="${LOG_DIRECTORY}${BASE_JOB_NAME}" -v jobs=$NUM_JOBS "append.qsub"
